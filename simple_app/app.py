e12422c48f8e31427252908160af1c8e3fc4e152
AndrewSLowe <andrewstevenlowe@gmail.com>
2020-03-23 18:34:55 -0400

    changed some stuff

--- simple_app/app.py
+++ simple_app/app.py
@@ -10,10 +10,9 @@ def create_app():
     @app.route('/recommend', methods=['POST'])
     def recommended():
 
-        baseline_time = jsonify({
-      "optimal_time": "1PM"
-})
-        r = requests.put(backend_url, json=baseline_time)
+        baseline_time = jsonify({"optimal_time": "1PM"})
+        
+        r = requests.put(backend_url, data=baseline_time)
         return "worked"
 
     return app
