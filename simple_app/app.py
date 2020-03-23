a432f064515c7cf9c1c5422021d12ab375ebbbb2
AndrewSLowe <andrewstevenlowe@gmail.com>
2020-03-23 18:19:55 -0400

    chaning the app.py file

--- simple_app/app.py
+++ simple_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
 import requests
+from flask import Flask, request, jsonify
 
 backend_url = 'https://post-route-feature.herokuapp.com/api/posts/:id'
 
@@ -7,13 +7,13 @@ def create_app():
     app = Flask(__name__)
     
 
-    @app.route('/recommend', methods=['POST', 'GET'])
+    @app.route('/recommend', methods=['POST'])
     def recommended():
 
         baseline_time = jsonify({
       "optimal_time": "1PM"
 })
-        r = requests.put(backend_url, data=baseline_time)
-        return r
+        r = requests.put(backend_url, json=baseline_time)
+        return "worked"
 
     return app
