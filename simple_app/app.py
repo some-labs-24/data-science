709ac4462fad17816f5d8fbc8f1844d5c3cf8b3a
AndrewSLowe <andrewstevenlowe@gmail.com>
2020-03-23 17:45:53 -0400

    deleted requirements.txt and added PUT request

--- simple_app/app.py
+++ simple_app/app.py
@@ -1,16 +1,18 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
+
+backend_url = 'https://post-route-feature.herokuapp.com/api/posts/:id'
 
 def create_app():
     app = Flask(__name__)
-   
-
     
 
-    @app.route('/recommand',methods=['GET'])
-    def recommanded():
+    @app.route('/recommend', methods=['POST'])
+    def recommended():
 
-        return jsonify({
-      "recommended_time": "1PM"
+        baseline_time = jsonify({
+      "optimal_time": "1PM"
 })
+
+        return request.put(url=backend_url, data=baseline_time)
 
     return app
