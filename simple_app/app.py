6a09bcf83a04f3c6ecb224a17aa9093063eca7e1
Jud Taylor <jud.taylor@protonmail.com>
2020-04-06 21:53:57 -0400

    testing auth

--- simple_app/app.py
+++ simple_app/app.py
@@ -5,8 +5,7 @@ import datetime
 from functools import wraps
 
 # token_expected = 'access_token myToken' # Supplied by backend team
-token_expected_by_backend = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyOSwiZW1haWwiOiJkczEwQGxhc2Vyc2hhcmtzLmNvbSIsIm9rdGFfdXNlcmlkIjoiRFMgaGF2ZSBubyBPa3RhIiwiaWF0IjoxNTg1Njk5MTU3LCJleHAiOjE1ODgyOTExNTd9.V7lAN61UrWZRDwJg-V9Xuy8G9Bw4EVyHkYVW1xGfLPA' # Supplied by backend team
-
+token_expected_by_backend = 'not saved in github'
 
 def token_required(func_to_wrap):
     """Decorator used to apply 'requirement for valid token 
