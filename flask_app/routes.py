46c15d70086117e74ba8250d55b8aa757fa02db3
Andrew Lowe <54857972+AndrewSLowe@users.noreply.github.com>
2020-04-18 18:37:14 -0400

    Fix deploy (#5)

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -4,6 +4,11 @@ from .optimize_time import data_wrangling   # Look into relative imports
 import datetime
 import logging
 import requests 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+BACKEND_AUTHORIZATION = os.getenv("BACKEND_AUTHORIZATION")
 
 recommend_time = Blueprint('recommend_time', __name__)
 
@@ -11,10 +16,10 @@ recommend_time = Blueprint('recommend_time', __name__)
 def recommendation():
     user_input = request.get_json()
     _id = user_input['id']
-    twitter_handle = user_input['screen_name']
+    twitter_handle = user_input['screenname']
 
     backend_url = 'https://social-media-strategy.herokuapp.com/api/posts/' + str(_id)
-    header_data = {'Authorization' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijo2LCJlbWFpbCI6ImRzMTBAbGFzZXJzaGFya3MuY29tIiwib2t0YV91c2VyaWQiOiJEUyBoYXZlIG5vIE9rdGEiLCJpYXQiOjE1ODYzNzg0OTgsImV4cCI6MTU4ODk3MDQ5OH0.MH8RkfEYcbpzIFchet4BZZJe74MhJjIQ5ZbYhsryAbw'}
+    header_data = {'Authorization' : BACKEND_AUTHORIZATION}
 
     dw = data_wrangling(twitter_handle, 5)
     fi = dw.followers_ids()
