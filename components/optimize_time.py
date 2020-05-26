4fc82598b22e666aaff0f7d0b13f55f33be77282
Lawrence Kimsey <lmkimsey@gmail.com>
2020-05-26 15:29:58 -0600

    Attempting to fix bug

--- components/optimize_time.py
+++ components/optimize_time.py
@@ -12,11 +12,8 @@ load_dotenv()
 
 TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
 TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
-TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
-TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
